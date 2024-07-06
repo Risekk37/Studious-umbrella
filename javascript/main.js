dd5fb4185dd71b25d55c6524a2e450870f1abaea
Risekk37 <ktkim8461@gmail.com>
2024-07-05 22:41:23 -0400

    s

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+console.log("this works");
+const APT_KEY = config.WEATHER_API_KEY;
+fetch(`http://api.openweathermap.org/data/2.5/weather?zip=10128&APPID=${WEATHER_API_KEY}`)
+  .then(response => response.json())
+  .then(data => console.log(data));
